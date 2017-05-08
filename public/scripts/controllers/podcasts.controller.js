6f45230762fa4bde0b553703a89d5d8bf8309dbf
wilkdasilk <anwilk92@gmail.com>
2017-05-08 13:28:44 -0700

    display and filter podcast index

---
+++ public/scripts/controllers/podcasts.controller.js
@@ -0,0 +1,24 @@
+(function() {
+
+  angular
+    .module('podBooth')
+    .controller('podcastsCtrl', podcastsCtrl);
+
+  podcastsCtrl.$inject = ['$http'];
+  function podcastsCtrl(   $http ) {
+    var vm = this;
+
+    vm.podcasts = [];
+
+    $http({
+      method: 'GET',
+      url: '/api/podcasts'
+    }).then(function (res) {
+      vm.podcasts = res.data;
+    }, function (err) {
+      console.log('There was an error getting the data', err);
+    });
+
+  }
+
+})();
--- seed.js
+++ seed.js
@@ -4,25 +4,25 @@ var podcastList =[];
 podcastList.push({
               name: 'The Tim Ferriss Show',
               description: `Tim Ferriss is a self-experimenter and bestselling author, best known for The 4-Hour Workweek, which has been translated into 40+ languages. Newsweek calls him "the world's best human guinea pig," and The New York Times calls him "a cross between Jack Welch and a Buddhist monk." In this show, he deconstructs world-class performers from eclectic areas (investing, chess, pro sports, etc.), digging deep to find the tools, tactics, and tricks that listeners can use.`,
-              website: 'tim.blog',
+              website: 'http://tim.blog',
               image: './images/timferriss.jpg'
             });
 podcastList.push({
               name: 'Philosophize This!',
               description: `Beginner friendly if listened to in order! For anyone interested in an educational podcast about philosophy where you don't need to be a graduate-level philosopher to understand it. In chronological order, the thinkers and ideas that forged the world we live in are broken down and explained.`,
-              website: 'philosophizethis.org',
+              website: 'http://philosophizethis.org',
               image: './images/philosophizethis.jpg'
             });
 podcastList.push({
               name: 'Longform Podcast',
               description: `A weekly conversation with a non-fiction writer on how they tell stories.`,
-              website: 'longform.org/podcast',
+              website: 'http://longform.org/podcast',
               image: './images/longform.jpg'
             });
 podcastList.push({
               name: 'StarTalk Radio',
               description: `Science, pop culture & comedy collide on StarTalk w/ astrophysicist & Hayden Planetarium director Neil deGrasse Tyson, comic co-hosts, celebrities & scientists.`,
-              website: 'startalkradio.net',
+              website: 'http://startalkradio.net',
               image: './images/startalk.jpg'
             });
 
