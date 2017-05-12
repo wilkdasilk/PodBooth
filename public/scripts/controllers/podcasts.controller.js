2c590185fb080fda9fdbddd0faf4e5964aac9c6f
wilkdasilk <anwilk92@gmail.com>
2017-05-11 17:40:04 -0700

    podcast hides subscribe button if already subscribed

--- public/scripts/controllers/podcasts.controller.js
+++ public/scripts/controllers/podcasts.controller.js
@@ -15,13 +15,13 @@
       method: 'GET',
       url: '/api/podcasts'
     }).then(function (res) {
+      console.log("got data");
       vm.podcasts = res.data;
     }, function (err) {
       console.log('There was an error getting the data', err);
     });
 
     vm.subscribe = function(podcast) {
-      console.log("in the subscribed function");
       $http({
         method: 'POST',
         url: `/api/podcasts/${podcast._id}/subscribe`,
@@ -29,8 +29,11 @@
           Authorization: 'Bearer ' + authentication.getToken()
         }
       }). then(function(res){
-        console.log("subscribed!");
       })
+    }
+
+    vm.subscribed = function(podcast) {
+      return podcast.subscribers.includes(authentication.currentUser()._id);
     }
 
   }
