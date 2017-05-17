376ceebfddbf7b115ce56edc116f4ee7381f63b8
wilkdasilk <anwilk92@gmail.com>
2017-05-16 19:59:44 -0700

    wrap socket in angular service

--- public/scripts/controllers/podcastShow.controller.js
+++ public/scripts/controllers/podcastShow.controller.js
@@ -4,12 +4,14 @@
     .module('podBooth')
     .controller('podcastShowCtrl', podcastShowCtrl);
 
-  podcastShowCtrl.$inject = ['$http', '$routeParams', '$location'];
-  function podcastShowCtrl(   $http,   $routeParams,   $location ) {
+  podcastShowCtrl.$inject = ['$http', '$routeParams', '$location', 'socket'];
+  function podcastShowCtrl(   $http,   $routeParams,   $location,   socket ) {
     var vm = this;
     vm.clickThru = false;
 
     vm.podcast = [];
+    vm.messages = [];
+
 
     $http({
       method: 'GET',
@@ -21,19 +23,17 @@
         //no podcast found
         $location.path('/podcasts');
       }
-      vm.socket = io.connect('/');
-      vm.socket.on('connect', function() {
-        vm.socket.emit('room', $routeParams.id);
-      });
-      vm.socket.on('announcements', function(data) {
+      socket.emit('room', $routeParams.id);
+      socket.on('announcements', function(data) {
         console.log('Got announcement:', data.message);
       });
-      vm.socket.emit('event', { message: "Hey it's a message!" });
-      vm.socket.on('stats', function(data) {
+      socket.emit('event', { message: "Hey it's a message!" });
+      socket.on('stats', function(data) {
         console.log('Connected clients:', data.numClients);
       });
-      vm.socket.on('message', function(data) {
+      socket.on('message', function(data) {
         console.log('got a message:', data.message);
+        vm.messages.push(data.message);
       });
     }, function (err) {
       console.log('There was an error getting the data', err);
