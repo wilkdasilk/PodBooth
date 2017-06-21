73799fa2f37eb9a44a56f652053201f120cc5384
wilkdasilk <anwilk92@gmail.com>
2017-06-20 17:34:39 -0700

    nav options restructure

--- public/scripts/controllers/navigation.controller.js
+++ public/scripts/controllers/navigation.controller.js
@@ -8,17 +8,6 @@
   function navigationCtrl(   $location,   authentication,   $scope) {
     var vm = this;
 
-    vm.dropDownConfig = function() {
-      return {
-        'inDuration': 300,
-        'outDuration': 225,
-        'constrain_width': false, // Does not change width of dropdown to that of the activator
-        'hover': true, // Activate on click
-        'alignment': 'left', // Aligns dropdown to left or right edge (works with constrain_width)
-        'gutter': 0, // Spacing from edge
-        'belowOrigin': true // Displays dropdown below the button
-      };
-    };
   }
 
 
